1399f7610923df2ed80f110c813e458dae6c8d74
geyandra <geyandra.g.k@gmail.com>
2022-11-17 08:22:07 +0700

    add landing page wireframe design

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:hms_16/Views/landing_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        
+      ),
+      home: LandingPage(),
+    );
+  }
+}
